72eff84704446f8a932a36d2ad682fb7c96ec3be
Repelstelertje <andreasalaerts@protonmail.com>
2025-06-18 18:21:54 +0200

    Refactor layout with header and footer partials

---
+++ footer.php
@@ -0,0 +1,35 @@
+    <footer class="py-5 bg-black">
+        <div class="container px-5 text-white">
+            <div class="row">
+                <div class="col-lg-3 mb-3">
+                    <p class="m-0 text-center small"><?php echo $companyName; ?> is een onderdeel van<br> Media 2 Connect B.V. Copyright &copy; <?php echo date('Y'); ?></p>
+                </div>
+                <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+                    <p>Contact:</p>
+                    <p>Email: info@e-notifyer.nl<br>
+                    Phone: +31 615408257</p>
+                    <a href="contact.php">Direct contact</a>
+                    </p>
+                </div>
+                <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+                    <p>Adres:</p>
+                    <p>Louis Couperusplein 2<br>
+                    2514HP Den Haag<br>
+                    Nederland</p>
+                    </p>
+                </div>
+                <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+                    <p>Legal:</p>
+                    <p><a href="privacy.php">Privacy policy</p>
+                    <p><a href="cookie.php">Cookie policy</p>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+</html>
